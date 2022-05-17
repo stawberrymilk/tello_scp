1b7347ca90cd7e44a4a59ecc4d646b9ccf891f73
stawberrymilk <105313050+stawberrymilk@users.noreply.github.com>
2022-05-17 10:58:04 -0700

    Update tello.py

--- tello.py
+++ tello.py
@@ -5,3 +5,16 @@ me = Tello.Tello()
 me.connect()
 
 print(me.get_battery())
+me= 80% up + 120% up
+
+me=  Move 23inch forward())
+
+ me= sleep= 1())
+
+ me= turn())
+
+ me= back 23inch())
+
+  me= sleep())
+ me=go down())
+ me=sleep
